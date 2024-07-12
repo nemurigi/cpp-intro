b996219d9440cd8edf275a676f23b0469a3613aa
Nemurigi <ne6rigi@gmail.com>
2024-07-13 08:58:53 +0900

    fix socialLinks

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -50,7 +50,7 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/nemurigi/cpp-intro' }
     ],
 
     search: {
